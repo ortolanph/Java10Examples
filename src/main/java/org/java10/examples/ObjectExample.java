17afc540368c832681cb4a672dbdb93a8b1fd742
Paulo Henrique Ortolan <paulo.ortolan@serpro.gov.br>
2018-04-05 18:20:37 -0300

    Updating example with a list example

--- src/main/java/org/java10/examples/ObjectExample.java
+++ src/main/java/org/java10/examples/ObjectExample.java
@@ -2,6 +2,7 @@ package org.java10.examples;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class ObjectExample {
 
@@ -12,7 +13,20 @@ public class ObjectExample {
 
         var formatter = DateTimeFormatter.ISO_DATE;
 
+        var stringList = new ArrayList<String>();
+
+        stringList.add("All");
+        stringList.add("Java");
+        stringList.add("developers");
+        stringList.add("have");
+        stringList.add("type");
+        stringList.add("inference");
+        stringList.add("now");
+        stringList.add("!");
+
+
         System.out.println(string);
         System.out.println(now.format(formatter));
+        System.out.println(stringList);
     }
 }
